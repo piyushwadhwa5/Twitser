7e64b39765c51e86f7bf938c7c0c54da56028ce4
piyushwadhwa5 <piyushwadhwa5@gmail.com>
2013-02-25 16:57:01 +0530

    Update TwitserCS/Default.aspx.cs

--- TwitserCS/Default.aspx.cs
+++ TwitserCS/Default.aspx.cs
@@ -8,8 +8,8 @@ using Twitterizer;
 
 public partial class _Default : System.Web.UI.Page
 {
-    string ckey="qWBYJCeE71d9Yhqgt6JDQ";
-    string csec="eofIL3PIPFdnfTIS7HGh4CNU0vwOkmqZyvgo24dA";
+    string ckey="Your_Client_key";
+    string csec="Your Client Secret";
     string cba="http://127.0.0.1:1213/TwitserCS/redirect.aspx";
     protected void Page_Load(object sender, EventArgs e)
     {
